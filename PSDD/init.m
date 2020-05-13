25e95a5b8023558961d9558f92a2297a16057fcb
ysq <2570625004@qq.com>
2020-05-13 17:38:32 +0800

    debug model

--- PSDD/init.m
+++ PSDD/init.m
@@ -1,7 +1,7 @@
 load coe.mat;
 
 %some coefficient and constant
-K = 3.5;%PNG coefficient
+K = 4;%PNG coefficient
 S = 0.03;%m2, reference area
 g = 9.8;
 
@@ -10,7 +10,7 @@ thrust_time_idx = [0 4 12];%s, [thrust_start, boost_thrust_end, thrust_end]
 thrust = [40000 20000 0];%N, [boost_thrust,thrust,zero_trust]
 
 %mass property
-mass = [200 170 130]; %kg, [total_mass, mass_after_boost_thrust, mass_after_thrust]
+mass = [200 170 130];%kg, [total_mass, mass_after_boost_thrust, mass_after_thrust]
 
 %terminate condition
 t_max = 100;%s, max flying time of missile
@@ -19,7 +19,7 @@ h_max = 25000;%m, max flying height of missile
 h_min = 0;%m, min flying height of missile
 vm_term = 400;%m/s, min termination speed of missile
 vmt_term = -150;%m/s, min closing speed of missile and target
-r_damage = 20;%m, damage radius of missile
+r_damage = 50;%m, damage radius of missile
 
 %seeker
 r_seeker_active = 20000;%m, seeker active range of missile
