8ed88b5ef9e50838d76877df1711c7facc654936
ysq <2570625004@qq.com>
2020-06-29 01:13:34 +0800

    complete throw mode

--- missile/PSDD/init.m
+++ missile/PSDD/init.m
@@ -1,4 +1,4 @@
-load coe.mat;
+load('coe.mat');
 
 %some coefficient and constant
 K = 4;%PNG coefficient
@@ -7,13 +7,13 @@ g = 9.8;
 
 %engine property
 thrust_time_idx = [0 4 12];%s, [thrust_start, boost_thrust_end, thrust_end]
-thrust = [40000 20000 0];%N, [boost_thrust,thrust,zero_trust]
+thrust = [35000 18000 0];%N, [boost_thrust,thrust,zero_trust]
 
 %mass property
 mass = [200 170 130];%kg, [total_mass, mass_after_boost_thrust, mass_after_thrust]
 
 %terminate condition
-t_max = 100;%s, max flying time of missile
+t_max = 200;%s, max flying time of missile
 t_min = 3;%s, min flying time of missile
 h_max = 25000;%m, max flying height of missile
 h_min = 0;%m, min flying height of missile
@@ -28,3 +28,13 @@ halfa_seeker_lock = 15;%degree, half locking angle of missile seeker
 %some constraints
 nn_lim = 40; %normal load constraint
 TGO_max = 200;%s,TGO constraint
+
+overload_height = [0 3000 6000 7000 9000 12000 15000 25000];%m, typical height
+overload = [9 9 7.5 7 6 5 4 2];%g, maneuver overload of typical height
+
+turn_omega_height = [0 3048 6096 9144 12192];%m, typical height
+turn_omega = [18.5 14.3 10.2 6.7 3.9];%degree/s, turnning angular velocity of typical height
+
+%v:[0,inf], theta:(-pi,pi], psi:(-pi,pi]
+init_t = [60000 5000 0 200 deg2rad(0) deg2rad(0)];
+init_m = [0 5000 0 180 0 deg2rad(0)];
---
+++ missile/PSDD/test.m
@@ -0,0 +1,8 @@
+h=5000;
+t=20;
+tt=15;
+angle=0/57.3;
+%% 
+figure(1);
+plot(theta)
+grid on
